eaa1d9658b9c78e3fa4db98a22525c847bafd244
Wellington <wel.souza88@gmail.com>
2023-04-12 13:09:05 -0300

    Getting a user by id

--- src/main/java/com/wellington/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wellington/workshopmongo/resources/UserResource.java
@@ -1,12 +1,12 @@
 package com.wellington.workshopmongo.resources;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,16 +17,24 @@ import com.wellington.workshopmongo.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDto>>  findAll() {
-		
-		List<User> list = service.findAll() ;
+	public ResponseEntity<List<UserDto>> findAll() {
+
+		List<User> list = service.findAll();
 		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDto> findById(@PathVariable String id) {
+
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDto(obj));
 
 	}
 
