c315a3c6ff0c0fd936cac0d26d30700572c3aa44
Wellington <wel.souza88@gmail.com>
2023-04-13 13:32:08 -0300

    Creating entity Post with nested User

--- src/main/java/com/wellington/workshopmongo/config/Instantiation.java
+++ src/main/java/com/wellington/workshopmongo/config/Instantiation.java
@@ -1,32 +1,47 @@
 package com.wellington.workshopmongo.config;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.wellington.workshopmongo.domain.Post;
 import com.wellington.workshopmongo.domain.User;
+import com.wellington.workshopmongo.repository.PostRepository;
 import com.wellington.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
-	
+
+	@Autowired
+	private PostRepository postRepository;
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy").withZone(ZoneId.of("GMT"));
+
 		userRepository.deleteAll();
-		
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar para São Paulo",
+				maria);
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", fmt), "Bom dia!", "Acordei feliz hoje!", maria);
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 
 }
